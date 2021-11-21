b12d69f55acb1c15bf007964ddf1dd8bfb43cde0
pragyan-satapathy <pragyans248@gmail.com>
2021-11-21 18:53:33 +0530

    try to hiding react from devtools source and .env file added

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component  {
   }
   async getPyhton2Code() {
     try {
-      const response = await fetch('http://removedoptparse-env.eba-evqeqawt.us-east-2.elasticbeanstalk.com/convert', {
+      const response = await fetch(process.env.REACT_APP_CONVERTER, {
         method: 'POST',
         body: this.state.python_3_code
       });
@@ -41,21 +41,24 @@ class App extends Component  {
         <Toolbar style={{ background: '#2E3B55',color: 'white' }}>
           <Typography variant="h5">Python Code Converter</Typography>
         </Toolbar>
+        <Box px={3}  style={{ background: '#f5f5f5' }}>
+          <Grid container alignItems = "center">
+            <Grid item xs={5} align = "center">
+                <h3>Python-3</h3>
+            </Grid>
+            <Grid item xs={2} align = "center">
+              <Button variant="contained" color="primary" size="small" onClick={this.getPyhton2Code}>
+                CONVERT
+              </Button>
+            </Grid>
+            <Grid item xs={5} align = "center">
+                <h3>Python-2</h3>
+            </Grid>
+          </Grid>
+        </Box>
         <Grid container style={{ background: '#f5f5f5', height: '100vh', }}>
           <Grid item xs={6}>
-            <Box p={3}>
-              <Grid container spacing={3}>
-              <Grid item>
-                <h3>Python-3</h3>
-              </Grid>
-              <Grid item>
-                <Box mt={2}>
-                <Button variant="contained" color="primary" size="small" onClick={this.getPyhton2Code}>
-                  CONVERT
-                </Button>
-                </Box>
-              </Grid>
-              </Grid>
+            <Box p={3} pt={0}>
               <TextField
                 m = {3}
                 multiline
@@ -73,8 +76,7 @@ class App extends Component  {
             </Box>
           </Grid>
           <Grid item xs={6}>
-            <Box p={3}>
-              <h3>Python-2</h3>
+            <Box p={3} pt={0}>
               <TextField
                 m = {3}
                 multiline
