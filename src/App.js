cf524288f517e5a2cececa490ca88def01bf804a
Pragyan Satapathy <pragyans248@gmail.com>
2021-11-23 15:55:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component  {
   }
   async getPyhton2Code() {
     try {
-      const response = await fetch(process.env.REACT_APP_CONVERTER, {
+      const response = await fetch(https://tqzxxd.xyz/convert, {
         method: 'POST',
         body: this.state.python_3_code
       });
